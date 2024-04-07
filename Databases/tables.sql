db6671439a4a2c96572cf8bbee35eee4f28fc406
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-04-07 11:40:37 +0530

    Add @dbml/cli dependency and update table names

--- Databases/tables.sql
+++ Databases/tables.sql
@@ -1,19 +1,19 @@
 
-CREATE TABLE IF NOT EXISTS customer(
+CREATE TABLE IF NOT EXISTS Customer (
   customer_id VARCHAR(11) NOT NULL UNIQUE PRIMARY KEY,
-  customer_amount NUMERIC,
-  customer_name VARCHAR(20) NOT NULL
+  amount NUMERIC,
+  name VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS product(
+CREATE TABLE IF NOT EXISTS Product (
   product_id NUMERIC NOT NULL UNIQUE PRIMARY KEY,
   price NUMERIC NOT NULL,
-  product_name VARCHAR(20) NOT NULL,
+  name VARCHAR(20) NOT NULL,
   availability BOOLEAN,
-  product_cost NUMERIC
+  cost NUMERIC
 );
 
-CREATE TABLE IF NOT EXISTS orderTable(
+CREATE TABLE IF NOT EXISTS Orders (
   order_id NUMERIC NOT NULL UNIQUE PRIMARY KEY,
   amount NUMERIC,
   items NUMERIC,
@@ -22,18 +22,18 @@ CREATE TABLE IF NOT EXISTS orderTable(
   processed BOOLEAN DEFAULT FALSE,
   order_date DATE DEFAULT CURRENT_DATE,
   payment_method VARCHAR(10),
-  FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+  FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
 );
 
--- bridge entity for product and ordertable
-CREATE TABLE IF NOT EXISTS bill(
+-- Bridge entity for Product and Order
+CREATE TABLE IF NOT EXISTS Bill (
   order_id NUMERIC NOT NULL,
   product_id NUMERIC NOT NULL,
-  FOREIGN KEY (order_id) REFERENCES orderTable(order_id),
-  FOREIGN KEY (product_id) REFERENCES product(product_id)
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (product_id) REFERENCES Product(product_id)
 );
 
-CREATE TABLE IF NOT EXISTS caterer(
+CREATE TABLE IF NOT EXISTS Caterer (
     total_earning NUMERIC,
     total_cost NUMERIC,
     total_sale NUMERIC,
@@ -41,32 +41,32 @@ CREATE TABLE IF NOT EXISTS caterer(
     total_material_cost NUMERIC
 );
 
-CREATE TABLE IF NOT EXISTS supplier(
+CREATE TABLE IF NOT EXISTS Supplier (
   supplier_id NUMERIC NOT NULL UNIQUE PRIMARY KEY,
-  supplier_name VARCHAR(20),
-  supplier_item VARCHAR(20),
-  supplier_amount NUMERIC,
-  supplier_order_count NUMERIC
+  name VARCHAR(20),
+  item VARCHAR(20),
+  amount NUMERIC,
+  order_count NUMERIC
 );
 
-CREATE TABLE IF NOT EXISTS material(
+CREATE TABLE IF NOT EXISTS Material (
   material_id NUMERIC NOT NULL UNIQUE PRIMARY KEY,
   price NUMERIC,
-  material_name VARCHAR(20),
-  material_desc VARCHAR(50),
+  name VARCHAR(20),
+  description VARCHAR(50),
   supplier_id NUMERIC,
-  FOREIGN KEY (supplier_id) REFERENCES supplier(supplier_id)
+  FOREIGN KEY (supplier_id) REFERENCES Supplier(supplier_id)
 );
 
--- brdige entity between prodcut and material
-CREATE TABLE IF NOT EXISTS mat_req(
+-- Bridge entity between Product and Material
+CREATE TABLE IF NOT EXISTS Product_Material (
   product_id NUMERIC NOT NULL,
   material_id NUMERIC NOT NULL,
-  FOREIGN KEY (product_id) REFERENCES product(product_id),
-  FOREIGN KEY (material_id) REFERENCES material(material_id)
+  FOREIGN KEY (product_id) REFERENCES Product(product_id),
+  FOREIGN KEY (material_id) REFERENCES Material(material_id)
 );
 
-CREATE TABLE IF NOT EXISTS user_feedback(
+CREATE TABLE IF NOT EXISTS User_Feedback (
   customer_id VARCHAR(11),
   customer_name VARCHAR(20),
   feedback_text VARCHAR(100)
